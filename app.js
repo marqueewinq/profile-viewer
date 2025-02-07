5127ca20e6c8513d0c63ec48318d3b2470722f23
marqueewinq <marqueewinq@gmail.com>
2025-02-07 16:22:13 +0400

    added intro

--- app.js
+++ app.js
@@ -413,4 +413,32 @@ app.controller("ProfileController", function ($scope, $sce, $window, $timeout) {
         a.click();
         URL.revokeObjectURL(url);
     };
+
+    // Initialize the visibility state from cookies or default to true
+    $scope.isTextVisible = getCookie('isTextVisible') === 'false' ? false : true;
+
+    // Function to toggle the visibility of the text
+    $scope.toggleTextVisibility = function() {
+        $scope.isTextVisible = !$scope.isTextVisible;
+        setCookie('isTextVisible', $scope.isTextVisible, 365); // Save the state to cookies
+    };
+
+    // Utility functions to manage cookies
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
 });
